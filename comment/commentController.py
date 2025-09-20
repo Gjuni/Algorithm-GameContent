ff68a24cdffc36a6e48329b83e38620f842742a1
Gjuni <kmj06178@gmail.com>
2025-09-20 13:16:56 +0900

    plz

--- comment/commentController.py
+++ comment/commentController.py
@@ -1,5 +1,27 @@
 from flask import Flask
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, redirect, url_for
+from config.mongodb import get_db
+from bson.objectid import ObjectId
+
 
 
 comment = Blueprint('commnet', __name__, url_prefix='/commnet')
+
+@comment.route("/<thread_id>")
+def add(thread_id):
+    convId = ObjectId(thread_id)
+    db = get_db("school")
+
+    doc = db['thread'].find_one({"_id": convId})
+    
+    if doc :
+        comments = request.form['comments']
+
+        if comments == "" :
+            # comment 가 존재하지 않습니다.
+            return redirect(url_for('thread.getOne', thread_id=thread_id))
+        else :
+            # comment 가 존재합니다.
+            threads = db['thread']
+
+            threads.update_one({"_id": convId}, {"$push": {"comments": {"comment": comments, "uploadDate": datetime.now(timezone.utc)"}}})
